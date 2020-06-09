0ce56f3ef18740c15a6a7d75ff210b6324f473e0
Valentina Prato <44057199+valup@users.noreply.github.com>
2020-06-09 16:02:49 -0300

    Elimino itree_max

--- libs/itree.h
+++ libs/itree.h
@@ -40,8 +40,6 @@ void itree_recorrer_bfs(ITree it, FuncionVisitante visit);
 
 int itree_altura(ITree it);
 
-double itree_max(ITree it);
-
 /////////
 int getBalance(ITree N);
 void print2DUtil(ITree root, int space);
