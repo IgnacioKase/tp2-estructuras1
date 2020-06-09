36f026df4e7aec9cf5c9ea9ad3da0d3b0596e1e2
Valentina Prato <44057199+valup@users.noreply.github.com>
2020-06-09 15:23:44 -0300

    Comentarios

--- libs/intervalo.c
+++ libs/intervalo.c
@@ -1,6 +1,10 @@
 #include "intervalo.h"
 
+/* Devuelve si el intervalo x es el minimo
+   entre x e y */
 int intervalo_min(double x[2], double y[2]) {
+    /* consideramos intervalo inimo al que comienza antes
+    (o termina antes en caso de tener el mismo comienzo) */
     return x[0] < y[0] || (x[0] == y[0] && x[1] < y[1]);
 }
 
@@ -9,9 +13,12 @@ void intervalo_imprimir(double intervalo[2]) {
 }
 
 int intervalo_es_valido(double intervalo[2]) {
+    /* el intervalo es valido siempre y cuando
+    el comienzo no supere al final */
     return intervalo[0] <= intervalo[1];
 }
 
+// Devuelve si hay interseccion entre x e y
 int intervalo_interseca(double x[2], double y[2]) {
     return x[0] <= y[1] && y[0] <= x[1];
 }
