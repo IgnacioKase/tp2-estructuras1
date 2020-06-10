e922c46ae37ae34ee4271a262ca5e80a23b5fb68
Valentina Prato <44057199+valup@users.noreply.github.com>
2020-06-10 18:25:20 -0300

    Comentarios

--- libs/intervalo.c
+++ libs/intervalo.c
@@ -8,10 +8,15 @@ int intervalo_min(double x[2], double y[2]) {
     return x[0] < y[0] || (x[0] == y[0] && x[1] < y[1]);
 }
 
+/* Funcion auxiliar para los recorridos dfs/bfs
+   para imprimir */
 void intervalo_imprimir(double intervalo[2]) {
     printf("[%f, %f] ", intervalo[0], intervalo[1]);
 }
 
+/* Chequea si un intervalo puede existir
+   se usa en el shell antes de hacer cualquier operacion
+   en caso de haber ingresado un intervalo */
 int intervalo_es_valido(double intervalo[2]) {
     /* el intervalo es valido siempre y cuando
     el comienzo no supere al final */
@@ -23,11 +28,13 @@ int intervalo_interseca(double x[2], double y[2]) {
     return x[0] <= y[1] && y[0] <= x[1];
 }
 
+// Devuelve el maximo entre dos enteros
 int max_i(int a, int b) {
     return a > b ? a : b;
 }
 
+/* Devuelve el maximo entre dos doubles
+   (punto flotante) */
 double max_d(double a, double b) {
     return a > b ? a : b;
 }
-
